07b883e77077eac36c225a68bfba7b98412ff1d4
Tricketteh <Tricketteh@gmail.com>
2025-02-11 18:42:07 +0300

    Test for controller

--- test/controllers/TaskControllerSpec.scala
+++ test/controllers/TaskControllerSpec.scala
@@ -1,38 +1,100 @@
 package controllers
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits._
 
 import models.TaskDTO
+import org.apache.pekko.stream.Materializer
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
-import play.api.mvc.Results
-import play.api.test.{FakeRequest, Helpers}
-import play.api.test.Helpers.baseApplicationBuilder.injector
-import play.api.test.Helpers.{contentType, defaultAwaitTimeout, status}
-import play.test.WithApplication
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.http.Status.{BAD_REQUEST, CREATED, NOT_FOUND, NO_CONTENT, OK}
+import play.api.libs.json.Json
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{DELETE, GET, POST, PUT, contentAsJson, defaultAwaitTimeout, status, stubControllerComponents}
 import services.TaskService
 
-class TaskControllerSpec extends PlaySpec with MockitoSugar with Results with ScalaFutures {
+class TaskControllerSpec extends PlaySpec with MockitoSugar with GuiceOneAppPerSuite with ScalaFutures {
 
-  lazy val taskService = mock[TaskService]
-  implicit lazy val executionContext = injector.instanceOf[ExecutionContext]
-  val controller = new TaskController(taskService, Helpers.stubControllerComponents())
+  implicit lazy val materializer: Materializer = app.materializer
 
-  "TaskController GET" should {
-    "return a list of users" in new WithApplication(){
-      val result = controller.getAllTasks().apply(FakeRequest())
-      status(result) mustBe Ok
-      contentType(result) mustBe Some("application/Json")
+  val mockTaskService: TaskService = mock[services.TaskService]
+  val controller = new TaskController(mockTaskService, stubControllerComponents())
+
+  "TaskController" should {
+
+    "return all tasks when no query parameter is provided" in {
+      val tasks = List(TaskDTO("Task 1", isCompleted = false), TaskDTO("Task 2", isCompleted = true))
+      when(mockTaskService.getAllTasks(None)).thenReturn(Future.successful(tasks))
+
+      val request = FakeRequest(GET, "/tasks")
+      val result = controller.getAllTasks()(request)
+
+      status(result) mustBe OK
+      contentAsJson(result) mustBe Json.toJson(tasks)
     }
-  }
 
-  "TaskController POST" should {
-    "create a new task" in new WithApplication() {
+    "return filtered tasks when query parameter is provided" in {
+      val tasks = List(TaskDTO("Completed Task", isCompleted = true))
+      when(mockTaskService.getAllTasks(Some(true))).thenReturn(Future.successful(tasks))
 
+      val request = FakeRequest(GET, "/tasks?completed=true")
+      val result = controller.getAllTasks()(request)
+
+      status(result) mustBe OK
+      contentAsJson(result) mustBe Json.toJson(tasks)
     }
+
+    "return task by ID if found" in {
+      val task = TaskDTO("Test Task", isCompleted = false)
+      when(mockTaskService.getTaskByID("validId")).thenReturn(Future.successful(Some(task)))
+
+      val request = FakeRequest(GET, "/tasks/validId")
+      val result = controller.getTaskById("validId")(request)
+
+      status(result) mustBe OK
+      contentAsJson(result) mustBe Json.toJson(task)
+    }
+
+    "return 404 if task by ID is not found" in {
+      when(mockTaskService.getTaskByID("invalidId")).thenReturn(Future.successful(None))
+
+      val request = FakeRequest(GET, "/tasks/invalidId")
+      val result = controller.getTaskById("invalidId")(request)
+
+      status(result) mustBe NOT_FOUND
+    }
+
+    "delete an existing task" in {
+      when(mockTaskService.deleteTask("taskId")).thenReturn(Future.successful(true))
+
+      val request = FakeRequest(DELETE, "/tasks/taskId")
+      val result = controller.deleteTask("taskId")(request)
+
+      status(result) mustBe NO_CONTENT
+    }
+
+    "return 404 when deleting a non-existing task" in {
+      when(mockTaskService.deleteTask("taskId")).thenReturn(Future.successful(false))
+
+      val request = FakeRequest(DELETE, "/tasks/taskId")
+      val result = controller.deleteTask("taskId")(request)
+
+      status(result) mustBe NOT_FOUND
+    }
+
+    "delete all completed tasks" in {
+      when(mockTaskService.deleteCompletedTasks()).thenReturn(Future.successful(true))
+
+      val request = FakeRequest(DELETE, "/tasks/completed")
+      val result = controller.deleteCompletedTasks()(request)
+
+      status(result) mustBe NO_CONTENT
+    }
+
   }
 
 }
